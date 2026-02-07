4a290f137579fb99163b68a9ed5b404cd1c6ac84
Joby Francis <jobyjfrancis@gmail.com>
2026-02-08 11:52:56 +1300

    terraform configuration

--- containerized-LMS-migration/terraform/main.tf
+++ containerized-LMS-migration/terraform/main.tf
@@ -151,5 +151,98 @@ resource "aws_ecs_cluster" "edutech_lms_cluster" {
   }
 }
 
+resource "aws_ecs_task_definition" "edutech_lms_task_def" {
+  family                   = "EduTech-LMS-Task-Def"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "512" # 0.5 vCPU
+  memory                   = "1024" # 1 GB
+  execution_role_arn       = aws_iam_role.edutech_ecs_task_role.arn
+  task_role_arn            = aws_iam_role.edutech_ecs_task_role.arn
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "lms-frontend"
+      image     = "***************.dkr.ecr.ap-southeast-2.amazonaws.com/edutech-lms-frontend"
+      cpu       = 256 # 0.25 vCPU
+      memory    = 512 # 0.5 GB
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+  tags = {
+    Name = "EduTech-LMS-Task-Def"
+  }
+}
+
+resource "aws_lb" "edutech_lms_alb" {
+  name               = "EduTech-LMS-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.edutech_alb_sg.id]
+  subnets            = module.edutech_vpc.public_subnets
+  ip_address_type    = "ipv4"
+  tags = {
+    Name = "EduTech-LMS-ALB"
+  }
+}
+
+resource "aws_lb_target_group" "edutech_lms_tg" {
+  name        = "EduTech-LMS-TG"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.edutech_vpc.vpc_id
+  ip_address_type = "ipv4"
+  health_check {
+    protocol = "HTTP"
+    path     = "/"
+  }
+  tags = {
+    Name = "EduTech-LMS-TG"
+  }
+}
+
+resource "aws_lb_listener" "edutech_lms_http" {
+  load_balancer_arn = aws_lb.edutech_lms_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.edutech_lms_tg.arn
+  }
+}
+
+resource "aws_ecs_service" "edutech_lms_service" {
+  name            = "EduTech-LMS-Service"
+  cluster         = aws_ecs_cluster.edutech_lms_cluster.id
+  task_definition = "${aws_ecs_task_definition.edutech_lms_task_def.family}:1"
+  launch_type     = "FARGATE"
+  platform_version = "LATEST"
+  scheduling_strategy = "REPLICA"
+  desired_count   = 1
+  network_configuration {
+    security_groups = [aws_security_group.edutech_container_sg.id]
+    subnets         = module.edutech_vpc.public_subnets
+    }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.edutech_lms_tg.arn
+    container_name   = "lms-frontend"
+    container_port   = 3000
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.edutech_ecs_service_role_policy
+  ]
+}
+
 
 
