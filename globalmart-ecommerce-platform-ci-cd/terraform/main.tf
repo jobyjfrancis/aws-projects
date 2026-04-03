df8c13f95ef822e7d6f7c110653e90874ccca5b6
Joby Francis <jobyjfrancis@gmail.com>
2026-04-03 15:40:05 +1300

    ec2 instance

--- globalmart-ecommerce-platform-ci-cd/terraform/main.tf
+++ globalmart-ecommerce-platform-ci-cd/terraform/main.tf
@@ -8,16 +8,13 @@ variable "subnet_id" {}
 variable "market_place_amazon_linux_ami_alias" {}
 
 data "aws_subnet" "default_subnet" {
-  id = var.subnet_id
+  id             = var.subnet_id
   default_for_az = true
 }
 
 resource "aws_security_group" "globalmart_web_sg" {
   name        = "Globalmart-SG"
   description = "Allow HTTP/HTTPS from anywhere and SSH from my IP"
-  
-  
-
   tags = {
     Name = "Globalmart-SG"
   }
@@ -45,9 +42,9 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.globalmart_web_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
+  from_port   = 22
   ip_protocol = "tcp"
-  to_port     = 443
+  to_port     = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
@@ -63,16 +60,30 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
 }
 
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "GlobalMart-WebServer"
 
-  instance_type = "t3.micro"
-  key_name      = var.keypair_name
-  ami_ssm_parameter = var.market_place_amazon_linux_ami_alias
-  monitoring    = true
-  subnet_id     = data.aws_subnet.default_subnet.id
-  vpc_security_group_ids = [aws_security_group.globalmart_web_sg.id] 
+  instance_type          = "t3.micro"
+  key_name               = var.keypair_name
+  ami_ssm_parameter      = var.market_place_amazon_linux_ami_alias
+  monitoring             = true
+  subnet_id              = data.aws_subnet.default_subnet.id
+  vpc_security_group_ids = [aws_security_group.globalmart_web_sg.id]
+  user_data              = <<-EOF
+  #!/bin/bash
+  exec > /var/log/user-data.log 2>&1
+  yum update -y
+
+  # Install Node.js v16
+  curl -sL https://rpm.nodesource.com/setup_16.x | bash -
+  yum install -y nodejs
+
+  # Install Nginx
+  amazon-linux-extras install nginx1 -y
+  systemctl start nginx
+  systemctl enable nginx"
+  EOF 
 
   tags = {
     Terraform   = "true"
